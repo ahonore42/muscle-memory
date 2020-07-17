565e276401b09b564a72b484b34948adfd59a9df
ahonore42 <a1honore80@gmail.com>
2020-07-17 17:07:55 -0400

    make 5 js variables

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// 5 variables of different data types
+// string
+// number
+// array
+// boolean
+// object (4 different key value pairs string, number, array, boolean)
+
 console.log("hello")
+
+// string
+let stringyBoi = "Hi, I'm a string."
+
+// number / integer
+let iveBecomeSoNum = 805
+
+// array
+let arr = ['I', 'tell', 'you', 'h\'what']
+
+// boolean
+let mrBools = true
+
+// object
+let whatsThis = {
+    dino: "trex",
+    howLong: 40,
+    eats: ['meat', 'other dinos', 'everything else'],
+    isDead: true
+}
